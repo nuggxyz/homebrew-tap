302b0c8752590bd3637e56c0321f73895763b1fb
dub6ix <90335716+dub6ix@users.noreply.github.com>
2023-03-25 10:27:32 -0500

    update

--- core.rb
+++ core.rb
@@ -1,14 +1,35 @@
-class Core < Formula:
-    desc "Core utilities"
-    homepage "    url "
-    version "2023-01-01"
-
-    depends_on :xcode
-
-    depends_on "just"
-
-    def install:
-        system "make", "install", "PREFIX=#{prefix}"
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Core < Formula
+    desc ""
+    homepage ""
+    url "core"
+    version "2023.03.25"
+    sha256 ""
+    license ""
+  
+    # depends_on "cmake" => :build
+  
+    def install
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      # Remove unrecognized options if warned by configure
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      system "./configure", *std_configure_args, "--disable-silent-rules"
+      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     end
-
-__END__
+  
+    test do
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test core`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
+    end
+  end
+  
