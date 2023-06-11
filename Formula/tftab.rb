2f7d620c43d2e2f64b1e52c03fb768fc5c0880ca
walter <iamwalteh@me.com>
2023-06-11 14:04:10 -0500

    tab bumper

--- Formula/tftab.rb
+++ Formula/tftab.rb
@@ -2,11 +2,16 @@ class Tftab < Formula
     desc "Runs terraform fmt but using tabs instead of spaces."
     homepage "https://github.com/nuggxyz/homebrew-tap"
 
-	version "0.0.14"
+	version "0.0.12"
 
     depends_on "tfenv"
 
     url "https://github.com/nuggxyz/tftab", :using => :git, :branch => "main", :release => "#{version}"
+
+	livecheck do
+		url :stable
+		regex(/^v?(\d+(?:\.\d+)+)$/i)
+	end
 
 	def install
 		lib.install "tftab.sh"
@@ -25,3 +30,30 @@ class Tftab < Formula
       assert_match /\t/, (testpath/"test.tf").read
     end
 end
+
+
+
+	# livecheck do
+	# 	url :stable
+	# 	regex(/^tftab[._-]v?(\d+(?:\.\d+)+)$/i)
+	# 	strategy :github_releases do |json, regex|
+	# 	  json.map do |release|
+	# 		next if release["draft"] || release["prerelease"]
+
+	# 		match = release["tag_name"]&.match(regex)
+	# 		next if match.blank?
+
+	# 		{
+	# 		  version: match[1],
+	# 		  url: release["html_url"]
+	# 		}
+	# 	  end.compact
+	# 	end
+	#   end
+
+	#   version livecheck
+
+
+
+	# # version "0.0.12"
+	# # sha256 "102157dcebc168744972a3433aea934aaaabd1a22c015c35dbdb68c7dbcbfce4"
