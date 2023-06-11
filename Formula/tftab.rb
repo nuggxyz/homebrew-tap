d382b2d605d8bc7ef14cd512bfff36bb87b98afe
walter <iamwalteh@me.com>
2023-06-11 13:15:18 -0500

    fix

--- Formula/tftab.rb
+++ Formula/tftab.rb
@@ -25,30 +25,3 @@ class Tftab < Formula
       assert_match /\t/, (testpath/"test.tf").read
     end
 end
-
-
-
-	# livecheck do
-	# 	url :stable
-	# 	regex(/^tftab[._-]v?(\d+(?:\.\d+)+)$/i)
-	# 	strategy :github_releases do |json, regex|
-	# 	  json.map do |release|
-	# 		next if release["draft"] || release["prerelease"]
-
-	# 		match = release["tag_name"]&.match(regex)
-	# 		next if match.blank?
-
-	# 		{
-	# 		  version: match[1],
-	# 		  url: release["html_url"]
-	# 		}
-	# 	  end.compact
-	# 	end
-	#   end
-
-	#   version livecheck
-
-
-
-	# # version "0.0.12"
-	# # sha256 "102157dcebc168744972a3433aea934aaaabd1a22c015c35dbdb68c7dbcbfce4"
