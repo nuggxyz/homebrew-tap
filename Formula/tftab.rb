c6528b8098474f6d719506dc0db99bcba0c8edc9
walter <iamwalteh@me.com>
2023-06-11 14:20:27 -0500

    all da ruby

--- Formula/tftab.rb
+++ Formula/tftab.rb
@@ -1,59 +1,49 @@
+# Defines the formula for the tftab brew package
 class Tftab < Formula
-    desc "Runs terraform fmt but using tabs instead of spaces."
-    homepage "https://github.com/nuggxyz/homebrew-tap"
+	# Description of the package
+	desc "Runs terraform fmt but using tabs instead of spaces."
 
+	# The homepage for the package, typically a GitHub URL or official project page
+	homepage "https://github.com/nuggxyz/homebrew-tap"
+
+	# The version of the package that this formula will install
 	version "0.0.12"
 
-    depends_on "tfenv"
+	# List of dependencies needed for this package, in this case, it depends on tfenv
+	depends_on "tfenv"
 
-    url "https://github.com/nuggxyz/tftab", :using => :git, :branch => "main", :release => "#{version}"
+	# The URL to the source code of the package. The :using => :git parameter tells Homebrew to use Git to get the source code.
+	# :branch => "main" specifies the branch to clone.
+	# :release => "#{version}" specifies the release to clone
+	url "https://github.com/nuggxyz/tftab", :using => :git, :branch => "main", :release => "#{version}"
 
+	# The livecheck block is used to check for newer versions of the package than what is defined in the formula.
+	# The url :stable symbol will cause livecheck to check the stable url for the formula.
+	# The regex is used to parse version numbers from strings. Here it looks for a string that starts with an optional 'v' followed by one or more digits separated by periods.
 	livecheck do
-		url :stable
-		regex(/^v?(\d+(?:\.\d+)+)$/i)
+	  url :stable
+	  regex(/^v?(\d+(?:\.\d+)+)$/i)
 	end
 
+	# The install block is called when the user runs 'brew install'.
+	# Here it installs the 'tftab.sh' script and creates a symlink to it in the bin directory.
 	def install
-		lib.install "tftab.sh"
-		bin.install_symlink lib/"tftab.sh" => "tftab"
-	  end
+	  lib.install "tftab.sh"
+	  bin.install_symlink lib/"tftab.sh" => "tftab"
+	end
 
-    test do
-      (testpath/"test.tf").write <<~EOS
-        resource "aws_s3_bucket" "bucket" {
-          bucket = "bucket"
-          acl    = "private"
-        }
-      EOS
+	# The test block is called when the user runs 'brew test'.
+	# This block should contain a test that checks the functionality of the installed software.
+	# Here it writes a small terraform script to a file, runs 'tftab' on it, and then checks that the file contains a tab character.
+	test do
+	  (testpath/"test.tf").write <<~EOS
+		resource "aws_s3_bucket" "bucket" {
+		  bucket = "bucket"
+		  acl    = "private"
+		}
+	  EOS
 
-      system "#{bin}/tftab", "#{testpath}/test.tf"
-      assert_match /\t/, (testpath/"test.tf").read
-    end
-end
-
-
-
-	# livecheck do
-	# 	url :stable
-	# 	regex(/^tftab[._-]v?(\d+(?:\.\d+)+)$/i)
-	# 	strategy :github_releases do |json, regex|
-	# 	  json.map do |release|
-	# 		next if release["draft"] || release["prerelease"]
-
-	# 		match = release["tag_name"]&.match(regex)
-	# 		next if match.blank?
-
-	# 		{
-	# 		  version: match[1],
-	# 		  url: release["html_url"]
-	# 		}
-	# 	  end.compact
-	# 	end
-	#   end
-
-	#   version livecheck
-
-
-
-	# # version "0.0.12"
-	# # sha256 "102157dcebc168744972a3433aea934aaaabd1a22c015c35dbdb68c7dbcbfce4"
+	  system "#{bin}/tftab", "#{testpath}/test.tf"
+	  assert_match /\t/, (testpath/"test.tf").read
+	end
+  end
